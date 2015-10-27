bb812ee5a637720e133829385054c845f41d8ddc
Geir Gåsodden <geir.gasodden@pythonia.no>
2015-10-27 10:47:56 +0100

    Initial commit

---
+++ views/helpers/myPoliticalPartyLink.js
@@ -0,0 +1,17 @@
+'use strict'
+
+function getMyParty (committees) {
+  var party
+  committees.forEach(function (committee) {
+    if (committee.role === 'Parti') {
+      party = committee
+    }
+  })
+  return party
+}
+
+module.exports = function (context) {
+  var myCommittees = context.data.root.politicians[context.data.index].committees
+  var myParty = getMyParty(myCommittees)
+  return myParty ? '<a href="/parties/' + myParty.groupRecno + '">' + myParty.name + '</a>' : ''
+}
