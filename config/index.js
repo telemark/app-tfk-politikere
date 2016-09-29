46ecf1f6cd23549dbc187880e69459a174cac505
Geir Gåsodden <geir.gasodden@pythonia.no>
2016-09-29 08:14:46 +0200

    Changes default api-url

--- config/index.js
+++ config/index.js
@@ -2,5 +2,5 @@
 
 module.exports = {
   SERVER_PORT: process.env.SERVER_PORT || 8000,
-  API_URL: process.env.API_URL || 'http://192.168.99.100:3000'
+  API_URL: process.env.API_URL || 'https://politiker-api.t-fk.no'
 }
